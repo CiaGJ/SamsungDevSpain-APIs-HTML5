90c2b33fc1e81054d809b81f4bfd85a51246cabe
CiaGJ <lucia.gjim@gmail.com>
2023-05-15 15:31:44 +0200

    feat: uploading files

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 // File validation
 function fileValidation(){
-    let inputFile = document.getElementById("file")
+    var inputFile = document.getElementById("file")
     let fileType = inputFile.value
     let allowedExt = /(\.mp4)$/i
 
     if(!allowedExt.exec(fileType)){
         alert('Archivo no válido. Por favor, seleccione un archivo .mp4');
-        fileInput.value = '';
         return false;
     }
+}
+// File Upload process
+function fileLoader(){
+    var inputFile = document.getElementById("file").files[0]
+    var video = document.getElementById("video")
+    let reader = new FileReader()
+
+    reader.readAsDataURL(inputFile);
+    reader.onloadend =  ()=> {
+        video.setAttribute("src", reader.result);
+    };
+    
+}
+
+
+// Play button
+function playVideo(){
+    var video = document.getElementById("video")
+    var play = document.getElementById("play")
+    if(video.paused == false && video.ended == false){
+        video.pause()
+        play.innerHTML = "Reproducir"
+    }else{
+        video.play()
+        play.innerHTML = "Pausar"
+    }
+}
+
+// Volume up button
+function volumeUp(){
+    var video = document.getElementById("video")
+    video.volume +=0.1
+}
+
+// Volume down button
+function volumeDown(){
+    var video = document.getElementById("video")
+    video.volume -=0.1
 }
