45d57a7240ca584bea6b6feca0d31b13a00b1339
CiaGJ <lucia.gjim@gmail.com>
2023-05-15 16:56:44 +0200

    feat: last changes

--- index.js
+++ index.js
@@ -14,19 +14,36 @@ function fileValidation(){
     if(!allowedExt.exec(fileType)){
         alert('Archivo no válido. Por favor, seleccione un archivo .mp4');
         return false;
+    }else{
+        return true
     }
 }
 // File Upload process
 function fileLoader(){
+    if(fileValidation()){
     var inputFile = document.getElementById("file").files[0]
     var video = document.getElementById("video")
     let reader = new FileReader()
 
+    reader.onprogress = () =>{
+        let icon = document.getElementById("icon")
+        icon.setAttribute("class", "fa fa-circle-o-notch fa-spin")
+        alert("Carga en proceso")
+    }
+
     reader.readAsDataURL(inputFile);
     reader.onloadend =  ()=> {
         video.setAttribute("src", reader.result);
+        video.setAttribute("poster", "")
+        let icon = document.getElementById("icon")
+        icon.setAttribute("class", "fa fa-check-square")
+        let text = document.getElementById("buttonText")
+        text.style.backgroundColor = "green"
+        setVisible(play)
+        setVisible(document.getElementById("vol-up"))
+        setVisible(document.getElementById("vol-dowm"))
     };
-    
+    }
 }
 
 
@@ -37,9 +54,11 @@ function playVideo(){
     if(video.paused == false && video.ended == false){
         video.pause()
         play.innerHTML = "Reproducir"
+        play.setAttribute("class", "fa fa-play-circle playerBtn")
     }else{
         video.play()
         play.innerHTML = "Pausar"
+        play.setAttribute("class", "fa fa-pause-circle playerBtn")
     }
 }
 
@@ -53,4 +72,8 @@ function volumeUp(){
 function volumeDown(){
     var video = document.getElementById("video")
     video.volume -=0.1
+}
+
+function setVisible(btn){
+    btn.style.visibility = "visible"
 }
